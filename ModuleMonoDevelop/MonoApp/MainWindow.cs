b09ecb04e33500ac6add9f67451045493670574e
Aliaksandr Kananenka <Aliaksandr_Kananenka@epam.com>
2018-09-30 09:33:56 +0300

    Solving tasks.

---
+++ ModuleMonoDevelop/MonoApp/MainWindow.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Gtk;
+using ModuleStandart;
+
+public partial class MainWindow : Gtk.Window
+{
+    public MainWindow() : base(Gtk.WindowType.Toplevel)
+    {
+        Build();
+    }
+
+    protected void OnDeleteEvent(object sender, DeleteEventArgs a)
+    {
+        Application.Quit();
+        a.RetVal = true;
+    }
+
+    /// <summary>
+    /// Button click handler.
+    /// </summary>
+    /// <param name="sender">Sender.</param>
+    /// <param name="e">E.</param>
+    protected void OnButton3Pressed(object sender, EventArgs e)
+    {
+        var name = this.entry1.Text;
+        var dialog = new MessageDialog(this, DialogFlags.Modal, MessageType.Info, ButtonsType.Ok, "{0}", Greeting.GetGreeting(name));
+
+        dialog.Run();
+        dialog.Destroy();
+    }
+}
---
+++ ModuleVS/CoreApp/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using ModuleStandart;
+
+namespace CoreApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                if (args == null)
+                {
+                    throw new NullReferenceException("Input argument is null");
+                }
+
+                if (args.Length == 0 || args.Length > 1)
+                {
+                    throw new ArgumentOutOfRangeException($"Incorrect parameter {nameof(args)}");
+                }
+
+                OutputGreeting(args[0]);
+            }
+            catch (NullReferenceException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch (ArgumentOutOfRangeException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        /// <summary>
+        /// The output greeting.
+        /// </summary>
+        private static void OutputGreeting(string name)
+        {
+            var result = Greeting.GetGreeting(name);
+            Console.WriteLine(result);
+        }
+    }
+}
